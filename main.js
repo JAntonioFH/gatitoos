572763f303ed8e86d9b6bf7b1c71f2aab4ffe729
JAntonioFH <115757032+JAntonioFH@users.noreply.github.com>
2023-01-19 14:55:35 -0600

    o.o

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-console.log('Hello world')
+const APIURL= 'https://api.thecatapi.com/v1/images/search?limit=3';
 
-const URL= 'https://api.thecatapi.com/v1/images/search'
+async function reload(){
+    const res = await fetch(APIURL);
 
-fetch(URL)
-    .then(res=>res.json())
-    .then(data =>{
-        const img = document.querySelector('img')
-        img.src = data[0].url
         
-    })
+}
+
+// async function reload(){
+//     const res = await fetch(APIURL);
+//     const data = await res.json();
+//     for (let index = 0; index < data.length; index++) {
+//         let newImage = document.createElement("img");
+//         newImage.setAttribute('id','img'+index );
+//         newImage.setAttribute('width','300');
+//         newImage.src=data[index].url
+//         let parentDiv = document.getElementById("contenedor-fotos");
+//         parentDiv.appendChild(newImage);
+//     }        
+// }
+
+reload()
